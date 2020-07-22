816473da7dfd4645cf57c5b9b7430443c49e0c1a
Robert Gifford <robjgiff@gmail.com>
2020-07-23 00:26:46 +0100

    Scan for variations command

--- glue/ifnlScanVariations.js
+++ glue/ifnlScanVariations.js
@@ -5,21 +5,21 @@ var alignments = ["IFNLb1", "IFNLb2" ];
 // list variation -w "name like 'n-linked-glycosylation%'" 
 var variationTable;
 
+
+
 glue.logInfo("Processing sequence ID: "+id);
 
 glue.inMode("/alignment/"+alignment+"/"+alignmentName, function() {
 
-	var rowObjs = glue.tableToObjects(glue.command(["amino-acid","-f",gene,"-r"+refsequence]));
+	var rowObjs = glue.tableToObjects(glue.command(["variation","member","scan","-r","REF_IFNLb2_Mammal_MASTER","-f","glycosylation-region-1", "-v", "n-linked-glycosylation-o1"]));
 	for(var i = 0; i < rowObjs.length; i++) {
+  
+		var alignmentName       = rowObjs[i].alignmentName;
+		var sequenceID          = rowObjs[i].sequenceID;
+		var sufficientCoverage  = rowObjs[i].sufficientCoverage;
+		var present             = rowObjs[i].present;
 
-		var codon = rowObjs[i].codonNts;
-		var codonLabel = rowObjs[i].codonLabel;
-		var aminoAcid  = rowObjs[i].aminoAcid;
-		var relRefNt   = rowObjs[i].relRefNt;
-		glue.logInfo("Processing sequence ID: "+id+"Processing gene: "+gene+", got codon: "+codon+" Amino Acid "+aminoAcid);
-		var resultObj = rowObjs[i];
-		var codonIndex = gene + codonLabel;
-		geneResultMap[codonIndex] = resultObj;
+		glue.logInfo("Processing sequence ID: "+sequenceID+"COVERAGE: "+sufficientCoverage+", PRESENT: "+present);
 	
 	}		
 });
