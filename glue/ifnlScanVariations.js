7bbdd3ef7e205479b376226c3042b05becb92346
Robert Gifford <robjgiff@gmail.com>
2020-07-23 01:26:55 +0100

    Refactoring - match reference and alignment names using one consistent Gene ID

--- glue/ifnlScanVariations.js
+++ glue/ifnlScanVariations.js
@@ -1,25 +1,58 @@
-// List of genes to scan for
-var alignments = ["IFNLb1", "IFNLb2" ];
 
 // Get list of variations to scan for 
-// list variation -w "name like 'n-linked-glycosylation%'" 
 var variationTable;
+get_variations(variationTable);
 
+// Get list of alignments to scan in
+scan_for_variations(variationTable)
 
+// Get feature names for a give reference sequence
+function get_variations(variationTable) {
 
-glue.logInfo("Processing sequence ID: "+id);
-
-glue.inMode("/alignment/"+alignment+"/"+alignmentName, function() {
-
-	var rowObjs = glue.tableToObjects(glue.command(["variation","member","scan","-r","REF_IFNLb2_Mammal_MASTER","-f","glycosylation-region-1", "-v", "n-linked-glycosylation-o1"]));
+    // list variation -w "name like 'n-linked-glycosylation%'" 
+	var rowObjs = glue.tableToObjects(glue.command(["list","variation","-w","name like 'n-linked-glycosylation%'"]));
+	
 	for(var i = 0; i < rowObjs.length; i++) {
   
-		var alignmentName       = rowObjs[i].alignmentName;
-		var sequenceID          = rowObjs[i].sequenceID;
-		var sufficientCoverage  = rowObjs[i].sufficientCoverage;
-		var present             = rowObjs[i].present;
-
-		glue.logInfo("Processing sequence ID: "+sequenceID+"COVERAGE: "+sufficientCoverage+", PRESENT: "+present);
+        var rowObj = rowObjs[i]
+		var variationName = rowObj.name;
+		var featureName   = rowObj["featureLoc.feature.name"]
+		var refSeqName    = rowObj["featureLoc.referenceSequence.name"];
+		glue.logInfo("Processing variation: "+variationName+" in reference: "+refSeqName+", feature: "+featureName);
 	
+	    variationTable.set(featureName, rowObj);
+
 	}		
-});
+}
+
+// Scan for variations
+function scan_for_variations(variationTable) {
+
+	for(var i = 0; i < refseqs_to_scan.length; i++) {
+  
+        var rowObj = rowObjs[i]
+		var variationName = rowObj.name;
+		var featureName   = rowObj["featureLoc.feature.name"]
+		var refSeqName    = rowObj["featureLoc.referenceSequence.name"];
+		glue.logInfo("Processing variation: "+variationName+" in reference: "+refSeqName+", feature: "+featureName);
+	
+		// Scan in alignment for variation
+		glue.inMode("/alignment/"+alignment+"/"+alignmentName, function() {
+
+			var rowObjs = glue.tableToObjects(glue.command(["variation","member","scan","-r","REF_IFNLb2_Mammal_MASTER","-f","glycosylation-region-1", "-v", "n-linked-glycosylation-o1"]));
+			for(var i = 0; i < rowObjs.length; i++) {
+  
+				var alignmentName       = rowObjs[i].alignmentName;
+				var sequenceID          = rowObjs[i].sequenceID;
+				var sufficientCoverage  = rowObjs[i].sufficientCoverage;
+				var present             = rowObjs[i].present;
+
+				glue.logInfo("Processing sequence ID: "+sequenceID+"COVERAGE: "+sufficientCoverage+", PRESENT: "+present);
+	
+			}		
+		});
+
+	}		
+
+}
+
